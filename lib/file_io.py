a8454ed4847b191c7976f4508281b58986de03a1
jared <jaredrotich3@gmail.com>
2025-06-05 21:55:35 +0300

    initiated file operators

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    """Write content to a new .txt file"""
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to an existing .txt file"""
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as file:
+        file.write(eppend_content)
 
 def read_file(file_name):
-    pass
+    """Read and return content from a .txt file"""
+    with open(f"{file_name}.txt", mode="r", encoding="utf-8") as file:
+        return file.read()
